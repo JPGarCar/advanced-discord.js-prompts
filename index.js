b548acde6ee06fe438f266a3edcbaab7c018a24f
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2021-03-29 15:04:16 -0700

    All documents ready to go from our discord bot!

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    ListPrompt: require('./prompts/list-prompts'),
+    ChannelPrompt: require('./prompts/channel-prompt'),
+    MemberPrompt: require('./prompts/member-prompt'),
+    MessagePrompt: require('./prompts/message-prompt'),
+    NumberPrompt: require('./prompts/number-prompt'),
+    RolePrompt: require('./prompts/role-prompt'),
+    SpecialPrompt: require('./prompts/special-prompt'),
+    StringPrompt: require('./prompts/string-prompt'),
+    TimeOutError: require('./errors').TimeOutError,
+    CancelError: require('./errors').CancelError,
+};
---
+++ typedefs.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * Common data for all prompts.
+ * @typedef PromptInfo
+ * @property {String} prompt - the text prompt to send to user
+ * @property {import('discord.js').TextChannel} channel - the channel to send the prompt to
+ * @property {String} userId - the ID of the user to prompt
+ * @property {Number} [time=0] - the time in seconds to wait for the response, if 0 then wait forever
+ * @property {Boolean} [cancelable=true] - if the prompt can be canceled
+ */
+module.exports.test = '';
+
+/**
+ * An option for a list reaction picker prompt.
+ * @typedef PickerOption
+ * @property {String} name - name of this option
+ * @property {String} description - description for this option
+ * @property {String} emojiName - the name of the emoji (only unicode emojis allowed!)
+ */
