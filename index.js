ee4367fe6273aec42baba81d82f9fcf7f7a3fa94
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2021-03-31 12:55:20 -0700

    promptInfo fixes

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 'use strict';
 
+/**
+ * Common data for all prompts.
+ * @typedef PromptInfo
+ * @property {String} prompt - the text prompt to send to user
+ * @property {TextChannel | DMChannel} channel - the channel to send the prompt to
+ * @property {String} userId - the ID of the user to prompt
+ * @property {Number} [time=0] - the time in seconds to wait for the response, if 0 then wait forever
+ * @property {Boolean} [cancelable=true] - if the prompt can be canceled
+ */
+
+/**
+ * An option for a list reaction picker prompt.
+ * @typedef PickerOption
+ * @property {String} name - name of this option
+ * @property {String} description - description for this option
+ * @property {String} emojiName - the name of the emoji (only unicode emojis allowed!)
+ */
+
 module.exports = {
     ListPrompt: require('./prompts/list-prompts'),
     ChannelPrompt: require('./prompts/channel-prompt'),
